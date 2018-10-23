f8c73cfb2311c2765d8d1c7781cf8390f4245425
mebnoah <noahispas@gmail.com>
2018-10-23 18:09:45 +0200

    added config for tests to run again

--- springboot-rabbitmq-pub/src/main/java/com/anynines/config/RabbitMQConfiguration.java
+++ springboot-rabbitmq-pub/src/main/java/com/anynines/config/RabbitMQConfiguration.java
@@ -3,9 +3,14 @@ package com.anynines.config;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.Cloud;
 import org.springframework.cloud.CloudFactory;
@@ -14,6 +19,8 @@ import org.springframework.cloud.service.messaging.RabbitConnectionFactoryConfig
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import com.anynines.queue.Receiver;
 
 @Configuration
 public class RabbitMQConfiguration {
@@ -59,9 +66,37 @@ public class RabbitMQConfiguration {
 			return connectionFactory;
 		}
 	}
-	
+
 	@Profile("test")
 	public class TestConfig {
+
+		public static final String queueName = "spring-boot-queue";
+
+		@Bean
+		Queue queue() {
+			return new Queue(queueName, false);
+		}
+
+		@Bean
+		Binding binding(Queue queue, TopicExchange exchange) {
+			return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+		}
+
+		@Bean
+		SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+				MessageListenerAdapter listenerAdapter) {
+			SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+			container.setConnectionFactory(connectionFactory);
+			container.setQueueNames(queueName);
+			container.setMessageListener(listenerAdapter);
+			return container;
+		}
+
+		@Bean
+		MessageListenerAdapter listenerAdapter(Receiver receiver) {
+			return new MessageListenerAdapter(receiver, "receiveMessage");
+		}
+
 		@Bean
 		public ConnectionFactory rabbitConnectionFactory() {
 			CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
